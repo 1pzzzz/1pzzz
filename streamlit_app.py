67c703c16b9e23eed79bbab31372f2b9c3ecf29e
1pzzzz <nadipzal@gmail.com>
2025-05-27 08:02:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 1pzzz baloon")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.image("/4B959E2D-E7BA-4C04-918D-240CF9BBD6AC.jpeg") width = 1000)
