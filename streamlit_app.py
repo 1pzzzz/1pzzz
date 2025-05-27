a53499f482921628940ed146057a58f41901812c
1pzzzz <nadipzal@gmail.com>
2025-05-27 07:56:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 1pzzz baloon")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+
+st,title("Apkikasi minimalis")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angle:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
